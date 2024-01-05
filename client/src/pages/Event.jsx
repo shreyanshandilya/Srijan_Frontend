b156ce348749de2a5bf5733206939e1b0fdacb98
harsh singh <hs180002@gmail.com>
2024-01-05 17:48:10 +0530

    gradient in comming soon

--- client/src/pages/Event.jsx
+++ client/src/pages/Event.jsx
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar/navbar";
 import { motion } from "framer-motion";
 import Footer from "../components/Footer";
 import { animateScroll as scroll } from "react-scroll";
+import "../Events/Events.css";
 
 function Event() {
   useEffect(() => {
@@ -15,7 +16,7 @@ function Event() {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 1 }}
-        className="bg-[#17160e] flex justify-center h-[50vh] items-center  text-4xl md:text-6xl font-bold text-[#efede0]"
+        className="bg-[#17160e] flex justify-center h-[50vh] items-center  text-4xl md:text-6xl font-bold text-[#efede0] gradient"
       >
         Coming Soon
       </motion.div>
