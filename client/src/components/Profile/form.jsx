d49cc227f2e22f92156a3d61f9ee416f61d22853
Ratul <cs.ratul03@gmail.com>
2024-01-07 22:54:21 +0530

    Profile Form

--- client/src/components/Profile/form.jsx
+++ client/src/components/Profile/form.jsx
@@ -5,6 +5,7 @@ import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Nav from "../Navbar/navbar";
+import { motion } from "framer-motion";
 import ProfilePage from "./profile";
 const InputDefault = styled(TextField)(({ theme }) => ({
   "& .MuiOutlinedInput-root": {
@@ -57,26 +58,26 @@ const Dashboard = () => {
   useEffect(() => {
     localStorage.setItem("email", Email);
   }, [Email]);
-  
+
   useEffect(() => {
     localStorage.setItem("phoneNumber", phoneNumber);
   }, [phoneNumber]);
-  
+
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
   };
-  
+
   const handlePhoneNumberChange = (event) => {
     setPhoneNumber(event.target.value);
   };
-  
-  const clearLocalStorage = ()=> {
+
+  const clearLocalStorage = () => {
     localStorage.setItem("email", "");
-    localStorage.setItem("phoneNumber" ,"");
+    localStorage.setItem("phoneNumber", "");
     setEmail("");
     setPhoneNumber("");
-    alert("user Logout" );
-  }
+    alert("user Logout");
+  };
 
   async function onSubmit(e) {
     e.preventDefault();
@@ -111,6 +112,7 @@ const Dashboard = () => {
         alignItems: "center",
         overflowY: "hidden",
       }}
+      className="bg-[#040d10]"
     >
       <Nav />
       {listItem.map((item) => (
@@ -131,11 +133,15 @@ const Dashboard = () => {
             color: "white",
           }}
         >
-          <div>
-            <Typography className="mb-3">
-              Enter your email id and phone no.
-            </Typography>
-            <form onSubmit={onSubmit} className="bold-form">
+          <div className="my-10 text-3xl text-[#dad3a5] font-semibold">
+            <motion.h2
+              initial={{ x: 50, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 0.5 }}
+            >
+              Enter your details
+            </motion.h2>
+            {/* <form onSubmit={onSubmit} className="bold-form">
               <div className="mb-5 mt-5">
                 <InputDefault
                   placeholder="XYZ"
@@ -181,9 +187,62 @@ const Dashboard = () => {
                   Logout
                 </Button>
               )}
-            </form>
+            </form> */}
           </div>
         </Container>{" "}
+        <motion.form
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 1, delay: 0.5 }}
+          onSubmit={(e) => onSubmit(e)}
+          className="max-w-sm mx-auto rounded-lg bg-[#dad3a5] flex-col justify-center shadow-xl px-5 py-5 backdrop-blur-lg"
+        >
+          <div className="mb-4">
+            <label
+              htmlFor="name"
+              className="block mb-2 text-sm font-medium text-[#040d10]"
+            >
+              Email
+            </label>
+            <input
+              type="text"
+              id="email"
+              className="bg-gray-50 border border-gray-300 text-[#040d10] text-sm rounded-lg focus:ring-[#0d0c06] focus:border-[#0d0c06] block w-full p-2.5"
+              placeholder="xyz@gmail.com"
+              label="Email"
+              name="email"
+              value={Email}
+              onChange={handleEmailChange}
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="phone"
+              className="block mb-2 text-sm font-medium text-[#040d10]"
+            >
+              Phone Number
+            </label>
+            <input
+              type="text"
+              id="phone"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#0d0c06] focus:border-[#0d0c06] block w-full p-2.5"
+              placeholder="9834XXXX23"
+              label="Phone Number"
+              name="number"
+              value={phoneNumber}
+              onChange={handlePhoneNumberChange}
+              required
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="text-[#efede0] bg-[#514c08]/60 hover:bg-[#efede0] hover:text-[#514c08] focus:ring-2 focus:outline-none focus:ring-[#514c08] font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center "
+          >
+            Submit
+          </button>
+        </motion.form>
       </div>
     </Fragment>
   );
