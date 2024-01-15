bfedbc198294c03d350aa3278c013162cb712b9e
shreyanshandilya <94351863+shreyanshandilya@users.noreply.github.com>
2024-01-15 14:13:57 +0530

    Update Merchandise.jsx

--- src/pages/Merchandise.jsx
+++ src/pages/Merchandise.jsx
@@ -520,7 +520,7 @@ function Merchandise() {
                 }}
               />{" "}
             </div>
-
+            {beta.type}
             <button
               type="submit"
               className="text-[#efede0] bg-[#514c08]/60 hover:bg-[#efede0] hover:text-[#514c08] focus:ring-2 focus:outline-none focus:ring-[#514c08] font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center "
