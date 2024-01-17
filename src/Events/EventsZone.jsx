0cd34e79c224faca9f18883d14f4f0a1c7e07cb9
Ratul <cs.ratul03@gmail.com>
2024-01-17 22:31:08 +0530

    event err resolve

--- src/Events/EventsZone.jsx
+++ src/Events/EventsZone.jsx
@@ -1,37 +1,42 @@
 // CardContainer.js
 
-import React from 'react';
-import Card from './Zonner_cards'; // Create a Card component if not already created
-import Navbar from '../components/Navbar/navbar';
+import React from "react";
+import Card from "./Zonner_cards"; // Create a Card component if not already created
+import Navbar from "../components/Navbar/navbar";
 import { motion } from "framer-motion";
-import img from './Pngs/bgg.png'
-const cardsData = [
-  0,0,0,0,0,0,0,0,0,0
-];
+import img from "./Pngs/bgg.png";
+const cardsData = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 const CardContainer = () => {
   return (
-    <div className="bg-cover bg-center bg-fixed" style={{backgroundImage:`url(${img})`}}>
-        <Navbar/>
-        <div className="p-5" style={{    borderRadius:" 7px",
-    
-    marginTop:"5rem",
-    backdropFilter: "blur(0.25px)"
-   }}>
-        <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1 }}
-        className="flex justify-center mb-[10px] items-center text-4xl md:text-6xl font-bold text-[#eab235]"
+    <div
+      className="bg-cover bg-center bg-fixed w-screen max-w-screen"
+      style={{ backgroundImage: `url(${img})` }}
+    >
+      <Navbar />
+      <div
+        className="p-5"
+        style={{
+          borderRadius: " 7px",
+
+          marginTop: "5rem",
+          backdropFilter: "blur(0.25px)",
+        }}
       >
-        Events
-      </motion.div>
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 items-center justify-items-center">
-      {cardsData.map((card, index) => (
-        <Card index={index} />
-      ))}
-    </div>
-    </div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1 }}
+          className="flex justify-center mb-[10px] items-center text-4xl md:text-6xl font-bold text-[#eab235]"
+        >
+          Events
+        </motion.div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 items-center justify-items-center">
+          {cardsData.map((card, index) => (
+            <Card index={index} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
